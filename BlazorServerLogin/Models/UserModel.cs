af60014bf2508d5244501d9b2655dd52588f8f99
William N <williamnguyen773@gmail.com>
2023-09-26 14:15:31 -0500

    changed employeeid to userid in db

---
+++ BlazorServerLogin/Models/UserModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerLogin.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "First name is required.")]
+        [Display(Name = "First Name")]
+        [StringLength(15, ErrorMessage = "First Name is too long.")]
+        [MinLength(3, ErrorMessage = "First Name is too short.")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [Display(Name = "Last Name")]
+        [StringLength(15, ErrorMessage = "Last Name is too long.")]
+        [MinLength(3, ErrorMessage = "Last Name is too short.")]
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 10, ErrorMessage = "You need to provide a long enough password.")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Confirmation of password is required.")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Your password and confirm password do not match.")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
--- LoginDB/dbo/Tables/Users.sql
+++ LoginDB/dbo/Tables/Users.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Users]
 (
-	[EmployeeId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),  
+	[UserId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),  
     [FirstName] NVARCHAR(50) NOT NULL,
     [LastName] NVARCHAR(50) NOT NULL,
     [PasswordHash] NVARCHAR(512) NOT NULL
